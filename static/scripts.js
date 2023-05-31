9ec9cb520f45f91474998443f67705fc4f6823ff
ochernyshov <uplooked@gmail.com>
2023-05-31 22:20:58 +0100

    1.0.2 - changed size of text and loading icon

--- static/scripts.js
+++ static/scripts.js
@@ -29,7 +29,7 @@ function submit(event) {
   const keyName = event.key;
   if(keyName != "Enter") return false;
   var query = $("#q").val();
-  $(".vacancies").html("<img alt='loading' width='6%' height='auto' style='margin: 10px auto;' src='/static/load.svg'/>");
+  $(".vacancies").html("<img alt='loading' height='64px' style='margin: 10px auto;' src='/static/load.svg'/>");
   var parameters = {
         q: query
   };
